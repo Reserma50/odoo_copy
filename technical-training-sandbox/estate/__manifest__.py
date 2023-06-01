15a6411b89adfd6cd2fc597fffd2d61cfc23af7d
Desarrollo Reserma <luis.dominguez@reserma.com>
2023-06-01 17:00:17 -0500

    change views almost 7

--- technical-training-sandbox/estate/__manifest__.py
+++ technical-training-sandbox/estate/__manifest__.py
@@ -9,11 +9,14 @@
     """,
     # data files always loaded at installation
     'data': [
-        #'views/mymodule_view.xml',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
         'security/ir.model.access.csv',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
         #'demo/demo_data.xml',
     ],
+    'application': True,
+
 }
