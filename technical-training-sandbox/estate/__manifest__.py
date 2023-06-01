cfdf4a5fe1e4bf28f06c5c973ec4240772dc2cd7
Desarrollo Reserma <luis.dominguez@reserma.com>
2023-06-01 10:26:15 -0500

    changes on tutorial 5

---
+++ technical-training-sandbox/estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real State",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Joseph González",
+    'category': 'App',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        #'views/mymodule_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        #'demo/demo_data.xml',
+    ],
+}
