20aea860b44c584843ca022bec7697cb7bae1ced
Desarrollo Reserma <luis.dominguez@reserma.com>
2023-06-19 15:04:10 -0500

    change viwes and forms

--- technical-training-sandbox/estate/__manifest__.py
+++ technical-training-sandbox/estate/__manifest__.py
@@ -10,9 +10,21 @@
     # data files always loaded at installation
     'data': [
         'security/ir.model.access.csv',
+
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+
         'views/estate_menus.xml',
+
         'views/estate_menu_tree.xml',
+        'views/estate_menu_tree_type.xml',
+        'views/estate_menu_tree_tag.xml',
+        'views/estate_menu_tree_offer.xml',
+
+        'views/estate_menu_form.xml', #uncomment this line!!
+        'views/estate_menu_view_tree_search.xml',
+
         
     ],
     # data files containing optionally loaded demonstration data
--- technical-training-sandbox/estate/models/estate_property.py
+++ technical-training-sandbox/estate/models/estate_property.py
@@ -28,9 +28,41 @@ class EstateProperty(models.Model):
         default='new',
         required=True,
         selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
-    )    
+    )   
+    # buller_id = fields.Many2one("res.Buller", string="Buller")
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    # partner_id = fields.Many2one("res.partner", string="Partner")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    seller_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    # An offer applies to one property, but the same property can have many offers.
+    offer_ids = fields.One2many("estate.property.offer","id", string="Offers")
 
     # def calculate_start_of_3_months(self):
     #     today = fields.Datetime.today()
     #     future_date = today + relativedelta(months=3)
     #     self.date_availability(default = self.env['ir.fields'].start_of('month', future_date))
+
+class EstatePropertyType(models.Model):
+    _name = "estate.property.type"
+    _description = "Estate Property Type"
+
+    name = fields.Char('Title',required=True)
+
+class EstatePropertyTag(models.Model):
+    _name = "estate.property.tag"
+    _description = "Estate Property Tag"
+
+    name = fields.Char('Title',required=True)
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offer"
+
+    price = fields.Float()
+    status = fields.Selection(
+        selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
+        copy = False,
+    )
+    partner_id = fields.Many2one("res.partner", required=True)
+    property_id = fields.Many2one("estate.property", required=True)
